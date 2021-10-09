e8b2b8b97cb92e2e84c35e797f1803dbeeedbb66
WendelSReis <90618369+WendelSReis@users.noreply.github.com>
2021-10-09 11:24:36 -0300

    teste

---
+++ Sp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long a,b;
+    cin>>a>>b;
+    long area=a*b;
+    long peri=2*(a+b);
+    if(area>peri)
+    {
+        cout<<"Area"<<endl;
+        cout<<area<<endl;
+    }
+    else if(area<peri)
+    {
+        cout<<"Peri"<<endl;
+        cout<<peri<<endl;
+    }
+    else
+    {
+        cout<<"Eq"<<endl;
+        cout<<area<<endl;
+    }
+    return 0;
+}
